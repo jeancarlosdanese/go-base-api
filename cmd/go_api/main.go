7f1351ed8b667513e20a9af4752841cb52335e20
Jean Carlos Danese <jean@danese.com.br>
2024-06-05 16:01:29 -0300

    Fixed MainTest errors on migrations apply.

--- cmd/go_api/main.go
+++ cmd/go_api/main.go
@@ -19,7 +19,7 @@ import (
 )
 
 // @title 							Swagger Go Base API
-// @version 						0.0.5
+// @version 						0.0.6
 // @description 					This is a Go Base API.
 // @termsOfService 					github.com/jeancarlosdanese/go-base-api/blob/main/LICENSE
 // @contact.name 					Go Base API Support
